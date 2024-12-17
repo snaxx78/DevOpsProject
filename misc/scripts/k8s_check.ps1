7d080a912b06761698cd92c01eb997e818d3072c
Snaxx <95628737+snaxx78@users.noreply.github.com>
2024-12-17 10:11:21 +0100

    feat : added ps1 scripts for k8s

---
+++ misc/scripts/k8s_check.ps1
@@ -0,0 +1,6 @@
+echo CHECK
+kubectl get pods
+kubectl get deployments
+kubectl get services
+kubectl get pvc
+kubectl get pv
---
+++ misc/scripts/k8s_deploy.ps1
@@ -0,0 +1,5 @@
+echo DEPLOY
+kubectl apply -f k8s/redis-pv.yaml
+kubectl apply -f k8s/redis-pvc.yaml
+kubectl apply -f k8s/service.yaml
+kubectl apply -f k8s/deployment.yaml
