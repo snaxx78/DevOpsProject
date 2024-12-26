912abaa5fae24b34a404dd073fec5ec45b3a9624
Snaxx <95628737+snaxx78@users.noreply.github.com>
2024-12-26 15:11:21 +0100

    fix :fixed  some bugs

--- userApi/src/dbClient.js
+++ userApi/src/dbClient.js
@@ -2,9 +2,6 @@ var redis = require("redis");
 const configure = require("./configure");
 
 const config = configure();
-
-console.log("Searching redis:", process.env.REDIS_HOST, process.env.REDIS_PORT);
-
 var db = redis.createClient({
   host: process.env.REDIS_HOST || config.redis.host,
   port: process.env.REDIS_PORT || config.redis.port,
@@ -12,6 +9,7 @@ var db = redis.createClient({
     console.log("Retrying connection to Redis...");
     return new Error("Retry time exhausted");
   },
+  
 });
 
 // Capture connection event
@@ -19,16 +17,13 @@ db.on("connect", function () {
   console.log("Connected to Redis successfully");
 });
 
-process.on("SIGINT", function () {
-  console.log("Closing Redis connection...");
-  db.quit();
-  process.exit();
+// Capture error event
+db.on("error", function (err) {
+  console.error("Redis connection error: " + err);
 });
 
-process.on("SIGTERM", function () {
-  console.log("Closing Redis connection...");
+process.on("SIGINT", function () {
   db.quit();
-  process.exit();
 });
 
 module.exports = db;
